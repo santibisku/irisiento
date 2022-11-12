c1ffcdc5f278d58d4110a25054c3a0487d53cb3e
santibisku <biskupovichs@gmail.com>
2022-11-12 14:09:33 -0300

    Initial commit

---
+++ productos.js
@@ -0,0 +1,20 @@
+const productos = [{
+        id: 1,
+        foto: "https://deliverind.com.ar/wp-content/uploads/2021/09/remera-oversize-x3-scaled.jpg",
+        nombre: "Remeras con diseño personalizado",
+        precio: 4500
+    },
+    {
+        id: 2,
+        foto: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/90ca6e105098489.5f71e4069d5e3.jpg",
+        nombre: "Ilustración",
+        precio: 5000
+    },
+    {
+        id: 3,
+        foto: src="../images/dibujosanti.jpg",
+        nombre: "Dibujo",
+        precio: 3000
+    },
+
+];
